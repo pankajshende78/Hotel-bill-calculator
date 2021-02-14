a9e87eba0b44a69434f3e1972a4f1177d72edfbc
pankajshende78 <shendepankaj78@gmail.com>
2021-02-14 17:43:14 +0530

    first commit

--- amtcalculator.js
+++ amtcalculator.js
@@ -1,14 +1,20 @@
-//Calculate Tip
+
 function calculateAmt() {
 var billAmt = document.getElementById("billamt").value;
   var serviceQual = document.getElementById("serviceQual").value;
   var numOfPeople = document.getElementById("peopleamt").value;
 
 
- if(billAmt===0 && numOfPeople==0 )
+if(numOfPeople==0 ){
+     
+     numOfPeople=1;
+  }
+
+ if(billAmt==0)
  {
-      alert("Please enter vaild details");
-    return;
+  
+  alert("Please enter vaild details");
+  return false;
  }
 
  var  total=billAmt/numOfPeople;
